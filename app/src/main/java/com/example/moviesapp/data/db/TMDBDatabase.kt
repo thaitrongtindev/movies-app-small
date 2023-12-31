73eee074cc54362d433467487bb8d9b548a63adc
thaitrongtin <thaitrongtindev@gmail.com>
2023-12-31 14:40:36 +0700

    inital inital TMDBDatabase

---
+++ app/src/main/java/com/example/moviesapp/data/db/TMDBDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.moviesapp.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.moviesapp.data.Movie
+
+@Database(entities = [Movie::class], version =  1, exportSchema = false)
+abstract class TMDBDatabase : RoomDatabase() {
+
+    abstract fun movieDao() : MovieDao
+}
