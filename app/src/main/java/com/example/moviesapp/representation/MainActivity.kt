f95d54ed317016ce476e8232a913ddc0af1af6aa
thaitrongtin <thaitrongtindev@gmail.com>
2023-12-31 17:53:41 +0700

    code ui main activity

--- app/src/main/java/com/example/moviesapp/representation/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/representation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.moviesapp
+package com.example.moviesapp.representation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moviesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
