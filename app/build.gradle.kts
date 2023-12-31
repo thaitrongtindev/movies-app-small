8633e0a65ae21afddb5a5f089ca37a40daaff74d
thaitrongtin <thaitrongtindev@gmail.com>
2023-12-31 13:56:27 +0700

    initial data class movie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
 }
 
 android {
@@ -33,9 +34,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        dataBinding = true
+    }
 }
 
 dependencies {
+
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -44,4 +49,39 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    //noinspection LifecycleAnnotationProcessorWithJava8
+    kapt ("androidx.lifecycle:lifecycle-compiler:2.6.2")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.2")
+//
+//
+    implementation ("androidx.room:room-runtime:2.6.1")
+   kapt ("androidx.room:room-compiler:2.6.1")
+   implementation("androidx.room:room-ktx:2.6.1")
+//
+//
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+//
+    implementation ("com.google.dagger:dagger:2.50")
+   annotationProcessor ("com.google.dagger:dagger-compiler:2.50")
+//
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+//
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+//
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+
+
+
+
+
+
+
+
 }
